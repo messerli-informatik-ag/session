ad3f81da10e543f82abf3318341bdd70eb9517b0
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-09-08 21:04:01 +0200

    Remove unused interface and implementation

--- Bash.Session/Infrastructure/ISessionInitializer.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Bash.Session.Infrastructure
-{
-    internal interface ISessionInitializer
-    {
-        Task<RawSession> InitializeSession(IRequest request);
-    }
-}
