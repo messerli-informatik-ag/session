d7727c79403eeecc99ff880bb607d6e60c7fab36
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-09-09 15:19:37 +0200

    Implement IResponse for ASP.NET Core

--- Bash.Session.AspNetCore/Internal/Response.cs
+++ Bash.Session.AspNetCore/Internal/Response.cs
@@ -1,3 +1,5 @@
+using System;
+using Bash.Session.Configuration;
 using Bash.Session.Http;
 using Microsoft.AspNetCore.Http;
 
@@ -14,12 +16,32 @@ namespace Bash.Session.AspNetCore.Internal
 
         public void SetCookie(Cookie cookie)
         {
-            throw new System.NotImplementedException();
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = cookie.Settings.HttpOnly,
+                Secure = MapSecurePreferenceToBool(cookie.Settings.SecurePreference),
+            };
+
+            _httpContext.Response.Cookies.Append(
+                cookie.Settings.Name.Value,
+                cookie.Value,
+                cookieOptions);
         }
 
         public void SetHeader(string name, string value)
         {
-            throw new System.NotImplementedException();
+            _httpContext.Response.Headers.Append(name, value);
+        }
+
+        private bool MapSecurePreferenceToBool(CookieSecurePreference securePreference)
+        {
+            return securePreference switch
+            {
+                CookieSecurePreference.Always => true,
+                CookieSecurePreference.Never => false,
+                CookieSecurePreference.MatchingRequest => _httpContext.Request.IsHttps,
+                _ => throw new InvalidOperationException(),
+            };
         }
     }
 }
